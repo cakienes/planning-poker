1f566dba2a74be09b4c59e484ae6a39a8564b02d
Cakienes <cakienes@gmail.com>
2019-06-09 03:35:10 +0300

    Updates

---
+++ precache-manifest.d0a58439e0368776a1ed7fee64362efe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26c0f7bb4f2ebc356a3ba938cde712a",
+    "url": "/planning-poker/index.html"
+  },
+  {
+    "revision": "a28ad5aab1dc3a013352",
+    "url": "/planning-poker/static/css/2.475c1a6b.chunk.css"
+  },
+  {
+    "revision": "3b2c9516bc2d88f407c5",
+    "url": "/planning-poker/static/css/main.e0e2aace.chunk.css"
+  },
+  {
+    "revision": "a28ad5aab1dc3a013352",
+    "url": "/planning-poker/static/js/2.f1f6440f.chunk.js"
+  },
+  {
+    "revision": "3b2c9516bc2d88f407c5",
+    "url": "/planning-poker/static/js/main.d8f53f4b.chunk.js"
+  },
+  {
+    "revision": "5616061573c6ca61febf",
+    "url": "/planning-poker/static/js/runtime~main.b5575156.js"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/planning-poker/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/planning-poker/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/planning-poker/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/planning-poker/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/planning-poker/static/media/open-iconic.eca2c267.svg"
+  }
+]);
